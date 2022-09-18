3da5254396abca8092476f7af71cd14f5f76efc1
Zdravko Georgiev <32423971+r00tmebaby@users.noreply.github.com>
2022-09-18 11:29:05 +0100

    Update programs.py

--- programs.py
+++ programs.py
@@ -1,5 +1,5 @@
 """
 #EXTM3U
 #EXTINF:-1 tvg-ID="24.kitchen.bg" tvg-name="BG: 24 Kitchen FHD" tvg-logo="https://logowik.com/content/uploads/images/386_24kitchen.jpg" group-title="Bulgaria",BG: 24 Kitchen FHD
-http://bestbuyiptv.tv:8080/live/r00tme/test/27894.m3u8
+http://bestbuyiptv.tv:8080/live/78/test/27894.m3u8
 """
